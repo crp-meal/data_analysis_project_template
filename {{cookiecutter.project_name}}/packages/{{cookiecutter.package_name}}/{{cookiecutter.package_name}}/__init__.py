5622c6b0b40ff64b43de7747afd99fecebb20d0c
crp-meal <carl.essman@collateralrepairproject.org>
2022-06-26 15:31:42 +0300

    update versioning in init file

---
+++ {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,8 @@
+from {{cookiecutter.package_name}}.config import config
+
+VERSION_PATH = config.PACKAGE_ROOT / 'VERSION'
+
+with open(VERSION_PATH, 'r') as version_file:
+    __version__ = version_file.read().strip()
+
+print('Running with version: ', __version__)
