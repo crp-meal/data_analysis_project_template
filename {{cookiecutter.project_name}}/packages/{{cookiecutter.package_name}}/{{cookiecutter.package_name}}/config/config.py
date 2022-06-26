f63a03ef1fd37e919a9d22a0d563d26702067994
crp-meal <carl.essman@collateralrepairproject.org>
2022-06-26 15:42:32 +0300

    setting up package configurations

--- {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/config/config.py
+++ {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/config/config.py
@@ -21,3 +21,80 @@ ROOT = PACKAGE_ROOT.parent
 INPUT_DIR = f"{PACKAGE_ROOT}/input"
 OUTPUT_DIR = f"{PACKAGE_ROOT}/output"
 CONFIG_FILE_PATH = ROOT/"config.yml"
+
+
+class PackageConfig(BaseModel):
+    """
+    Package information config.
+    """
+
+    package_name: str
+    raw_data: str
+    processed_data: str
+    preprocess_pipeline_name: Optional[str]
+    analysis_pipeline_name: Optional[str]
+    process_log_file: str
+
+
+class ModelConfig(BaseModel):
+    """
+    All configurations pertaining to
+    data processing and model fitting.
+    """
+
+    primary_dependent_variables: List[str]
+    variables_to_include: List[str]
+    random_state: int
+
+
+class Config(BaseModel):
+    ''' 
+    Core config object.
+    Collects config.yml, PackageConfig, ModelConfig.
+    '''
+
+    package_config: PackageConfig
+    model_config: ModelConfig
+
+
+def detect_config_file() -> Path:
+    '''
+    Identify configuration.yml.
+    '''
+    if CONFIG_FILE_PATH.is_file():
+        return CONFIG_FILE_PATH
+    else:
+        raise Exception(f"Config.yml not found in {CONFIG_FILE_PATH}")
+
+
+def extract_configurations_from_yml(config_path: Path = None) -> YAML:
+    '''
+    Parse YAML for package and model configurations.
+    '''
+
+    if not config_path:
+        config_path = detect_config_file()
+    if config_path:
+        with open(config_path, 'r') as config_file:
+            parsed_config_file = load(config_file.read())
+            return parsed_config_file
+    else:
+        raise OSError(f"No configuration fle found in {config_path}")
+
+
+def create_and_validate_configurations(parsed_config_file: YAML = None) -> Config:
+    '''
+    Run valdidation procedure for configuration information.
+    '''
+    if parsed_config_file is None:
+        parsed_config_file = extract_configurations_from_yml()
+
+    _config = Config(
+        package_config=PackageConfig(**parsed_config_file.data),
+        model_config=ModelConfig(**parsed_config_file.data)
+    )
+
+    return _config
+
+
+config = create_and_validate_configurations()
