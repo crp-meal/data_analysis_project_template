ca0060f2a30dbee9f741a815c99b6bc91a75d3e7
crp-meal <carl.essman@collateralrepairproject.org>
2022-06-26 16:20:14 +0300

    adding logging functionality

--- {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/main.py
+++ {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/main.py
@@ -1,13 +1,14 @@
 from {{cookiecutter.package_name}} import __version__ as _version
 from {{cookiecutter.package_name}}.config.config import config
+from {{cookiecutter.package_name}}.config.logging import ProcessLogger
 
 
 def main():
-    print(_version)
-    print(config.PACKAGE_ROOT)
+    ProcessLogger.processLogger.info(f" =============== Starting process with version: {_version} ===============")
 
     return 0
 
 
 if __name__ == "__main__":
     main()
+    ProcessLogger.processLogger.info(f" ========================== PROCESS END ========================== ")
