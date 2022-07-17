648c1f3b4316884e673bf204cb098e0954835dc4
crp-meal <carl.essman@collateralrepairproject.org>
2022-07-17 10:27:15 +0300

    import export data example

--- {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/main.py
+++ {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/main.py
@@ -1,11 +1,16 @@
 from {{cookiecutter.package_name}} import __version__ as _version
 from {{cookiecutter.package_name}}.config.config import config
 from {{cookiecutter.package_name}}.config.logging import ProcessLogger
+from {{cookiecutter.package_name}}.processing.data_manager import DataManager
+from {{cookiecutter.package_name}}.pipelines import pipelines
 
 ProcessLogger = ProcessLogger(__name__)
 
 def main():
     ProcessLogger.processLogger.info(f" =============== Starting process with version: {_version} ===============")
+
+    raw_data = DataManager.load_csv(file_name=config.package_config.raw_data)
+    DataManager.export_excel(data=raw_data, file_name=config.package_config.processed_data)
 
     return 0
 
