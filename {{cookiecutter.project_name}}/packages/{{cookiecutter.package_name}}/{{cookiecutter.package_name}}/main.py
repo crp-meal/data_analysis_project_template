43aa2f22400c5c1bb616f5c68731f360223bc051
crp-meal <carl.essman@collateralrepairproject.org>
2022-07-17 11:09:46 +0300

    logging for data processing

--- {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/main.py
+++ {{cookiecutter.project_name}}/packages/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/main.py
@@ -10,6 +10,12 @@ def main():
     ProcessLogger.processLogger.info(f" =============== Starting process with version: {_version} ===============")
 
     raw_data = DataManager.load_csv(file_name=config.package_config.raw_data)
+
+    ProcessLogger.processLogger.info(f"\n Raw data dimensions: \n num rows: {raw_data.shape[0]}, num cols: {raw_data.shape[1]}")
+    ProcessLogger.processLogger.info(f"Primary dependent variable(s): {config.model_config.primary_dependent_variables}")
+    ProcessLogger.processLogger.info(f"\n Preprocessing pipeline: \n {pipelines.preprocessing_pipeline}")
+
+
     processed_data = pipelines.preprocessing_pipeline.fit_transform(raw_data)
     DataManager.export_excel(data=processed_data, file_name=config.package_config.processed_data)
 
@@ -19,3 +25,4 @@ def main():
 if __name__ == "__main__":
     main()
     ProcessLogger.processLogger.info(f" ========================== PROCESS END ========================== ")
+
